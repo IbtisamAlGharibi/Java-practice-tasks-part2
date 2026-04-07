057c23ebb50fd0781d08f30fb67d560b9024a812
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 16:05:55 +0400

    Task 25: answerCell

---
+++ src/Task25.java
@@ -0,0 +1,21 @@
+public class Task25 {
+    public static void main(String[] args){
+        boolean isMorning = false;
+        boolean isMom = false;
+        boolean isAsleep = true;
+
+        if (isAsleep == true){
+            System.out.println("False");
+        } else if (isMorning == true) {
+            if (isMom == true) {
+                System.out.println("True");
+            } else {
+                System.out.println("False");
+            }
+        }
+
+        }
+
+
+    }
+
