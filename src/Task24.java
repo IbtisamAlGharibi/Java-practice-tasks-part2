7ce251b6222ebd5de48426582897f8742a7a9bf3
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 16:00:09 +0400

    Task 24: less20

---
+++ src/Task24.java
@@ -0,0 +1,13 @@
+public class Task24 {
+    public static void main(String[] args){
+
+        int n = 8;
+
+        if ( n % 20 == 18 ||  n % 20 == 19 ){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+
+    }
+}
