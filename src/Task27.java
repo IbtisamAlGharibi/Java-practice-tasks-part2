8718095e08eb713c9d1d5b19fecd404e2a2765f7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 16:27:14 +0400

    Task 27: inOrderEqual

---
+++ src/Task27.java
@@ -0,0 +1,24 @@
+public class Task27 {
+    public static  void main(String[] args){
+        int a = 0;
+        int b = 1;
+        int c = 2;
+        boolean equalOk = true;
+
+        if(equalOk = false){
+            if (a < b && b < c ){
+                System.out.println("True");
+            }
+        } else if (equalOk = true) {
+            if (a <= b && b <= c ){
+                System.out.println("True");
+            }
+
+        }else {
+            System.out.println("False");
+        }
+
+    }
+
+
+}
