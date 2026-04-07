8ed6b308d9e8e8614659c3a843e5d6da91d9fd91
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 15:57:50 +0400

    Task 23: specialEleven

---
+++ src/Task23.java
@@ -0,0 +1,12 @@
+public class Task23 {
+    public static void main(String[] args){
+        int n = 6;
+
+        if ( n % 11 == 0 ||  n % 11 == 1){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+
+    }
+}
