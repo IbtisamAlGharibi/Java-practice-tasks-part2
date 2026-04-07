fef8155f8d68bf8cf276b2b16533a939a31c75ed
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 15:19:12 +0400

    Task 17: inOrder

---
+++ src/Task17.java
@@ -0,0 +1,22 @@
+public class Task17 {
+    public static void main(String[] args){
+        int a =2;
+        int b = 4;
+        int c= 6;
+        boolean bOk = false;
+
+        if (bOk == false){
+            if (b > a && c > b){
+                System.out.println("True");
+            }
+        } else if (bOk == true) {
+            if (c > b ){
+                System.out.println("True");
+            }
+            
+        }else {
+            System.out.println("False");
+        }
+
+    }
+}
