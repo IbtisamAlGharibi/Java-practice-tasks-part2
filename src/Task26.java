5dcc0920622974c8b959ed7a91ef9397b99806c0
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 16:11:47 +0400

    Task 26: fizzString2

---
+++ src/Task26.java
@@ -0,0 +1,19 @@
+public class Task26 {
+    public static void main(String[] args0){
+
+        int n = 4;
+
+        if ( n % 3 == 0 &&  n % 5 == 0 ){
+            System.out.println("FizzBuzz!");
+        } else if (n % 3 == 0 ) {
+            System.out.println("Fizz!");
+
+        } else if ( n % 5 == 0 ) {
+            System.out.println("Buzz!"  );
+
+        }else {
+            System.out.println(n + "!");
+        }
+
+    }
+}
